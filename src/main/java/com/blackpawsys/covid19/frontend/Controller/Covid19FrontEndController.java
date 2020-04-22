ca559ab72f12c5a0d496dead12b3eb133d32dbcd
bopagep <pohodhika.bopage@uk.fujitsu.com>
2020-04-22 08:06:40 +0100

    change mapping

--- src/main/java/com/blackpawsys/covid19/frontend/Controller/Covid19FrontEndController.java
+++ src/main/java/com/blackpawsys/covid19/frontend/Controller/Covid19FrontEndController.java
@@ -57,7 +57,7 @@ public class Covid19FrontEndController {
   @Autowired
   private RestTemplate restTemplate;
 
-  @GetMapping(value = {"/", "/report", "/report/{direction}/{date}"})
+  @GetMapping(value = {"/", "/covid19", "/covid19/{direction}/{date}"})
   private String getDailyReport(Model model, @PathVariable Optional<Direction> direction, @PathVariable Optional<String> date) {
     String uri = allReportUri;
     LocalDate localDate = updateDates(model, direction, date);
@@ -144,7 +144,7 @@ public class Covid19FrontEndController {
     model.addAttribute("navigator", "yesterday");
   }
 
-  @GetMapping(value = {"/report/country/{country}"})
+  @GetMapping(value = {"/covid19/country/{country}"})
   private String getCountryReport(@PathVariable String country, Model model) {
     String uri = null;
     try {
